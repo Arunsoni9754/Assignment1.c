4a2fbf15a6494e2720b38e95ab07913753631c5c
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-13 22:57:15 +0530

    Create Question10.c

---
+++ Question10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int DD,MM,YYYY;
+
+    scanf("%d",&DD);
+    printf("/");
+    scanf("%d",&MM);
+    printf("/");
+    scanf("%d",&YYYY);
+    printf("Day-%d ,Month-%d ,Year-%d",DD,MM,YYYY);
+}
