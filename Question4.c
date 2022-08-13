4c69e74551eac83ddfbe1707cc4679fe5c224685
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-13 22:54:10 +0530

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float R,A;
+     
+    printf("Write the radius of circle:-");
+    scanf("%f",&R);
+    A=3.14*R*R;
+    printf("The Area of the cirle is:- %f",A);
+    return 0;
+}
